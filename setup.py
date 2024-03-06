df9a3df7966827678f85540389a425a1115f428c
Swaroop <Swaroop.rajlama2001@outlook.com>
2024-03-06 18:20:41 +0530

    Version updated with small fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tokenpy",
-    version='0.1.1',
+    version='1.0.0',
     author='byte-my-code',
     author_email='lamaswaroop@gmail.com',
     description='Check your wallet balance from the cli',

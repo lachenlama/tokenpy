aea2623a0e1ff1baa5cc92277bbd4ea75b259bd8
Swaroop <Swaroop.rajlama2001@outlook.com>
2024-03-12 18:11:53 +0530

    Smallfix

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name="tokenpy",
-    version='1.0.0',
+    version='1.0.1',
     author='byte-my-code',
     author_email='lamaswaroop@gmail.com',
     description='Check your wallet balance from the cli',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/lachenlama/tokenpy',
     packages=find_packages(),
